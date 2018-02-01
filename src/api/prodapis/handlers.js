52007830eb82a5c3728062f9eb729b547ccb6611
Shishir Chandra <shishir@bhangrarocks.com>
2018-02-01 23:37:09 +0530

    Created a new endpoint called csv2json which will accept a CSV file and return a JSON object.

--- src/api/prodapis/handlers.js
+++ src/api/prodapis/handlers.js
@@ -1,25 +1,49 @@
 let adder = (sum, element) => {
-	let p = new Promise ((resolve) => {
-    resolve(sum + element);
-  });
+    let p = new Promise((resolve) => {
+        resolve(sum + element);
+    });
 
-  return p;
+    return p;
 }
 
 
 export let loop = (request, h) => {
-  let numbers = [1,2,3,4,5,6,7,8,9,10];
-  let sum = 0;
-  
-  numbers.forEach(n => {
-    console.log(`Trying to add ${n}`);
-  	adder(sum, n)
-  		.then(res => {
-        console.log(`Current sum is ${n}`);
-        sum = res
-      });
-  });
-  
-  return sum;
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let sum = 0;
+
+    numbers.forEach(n => {
+        console.log(`Trying to add ${n}`);
+        adder(sum, n)
+            .then(res => {
+                console.log(`Current sum is ${n}`);
+                sum = res
+            });
+    });
+
+    return sum;
 };
 
+export let csv2json = (request, h) => {
+    // Initializing the CSV file to a variable.
+    var csv = request.payload.file;
+    // Splitting lines seperately.
+    var lines = csv.split("\n");
+    var result = [];
+    // Storing headers seperately.
+    var headers = lines[0].split(",");
+    // Iterating through the lines.
+    for (var i = 1; i < lines.length; i++) {
+        var obj = {};
+        var currentLine = lines[i].split(",");
+        // Checking for the line, should not be empty.
+        if (currentLine[0] !== '') {
+            for (var j = 0; j < headers.length; j++) {
+              // Putting each entry into the respective header.
+                obj[headers[j]] = currentLine[j];
+            }
+            result.push(obj);
+        };
+    }
+    // Returning the JSON result.
+    return result;
+};
--- src/api/prodapis/index.js
+++ src/api/prodapis/index.js
@@ -5,6 +5,12 @@ let routes = [
     method: 'GET',
     path: '/loop',
     handler: handlers.loop
+  },{
+  	// Adding a new endpoint to accept a CSV file and convert it to a json object.
+  	// It will accept a parameter called 'file', as a normal form data.
+  	method: 'POST',
+    path: '/csv2json',
+    handler: handlers.csv2json
   }
 ];
 
